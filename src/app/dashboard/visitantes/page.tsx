5ecb519cd274ebfcc7f7d7b04110b90b54277097
lcas21 <lucassanciro@gmail.com>
2024-05-16 10:28:19 -0300

    frist commit

---
+++ src/app/dashboard/visitantes/page.tsx
@@ -0,0 +1,10 @@
+import { Stack } from '@mui/material'
+import React from 'react'
+
+export default function Page(): React.JSX.Element{
+  return (
+    <Stack spacing={3}>
+    teste
+    </Stack>
+  )
+}
